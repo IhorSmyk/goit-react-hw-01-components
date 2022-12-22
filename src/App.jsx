9a2112d767eea1d6ac322233abfb4e760b601ff4
Ihor Smyk <ismyk256@gmail.com>
2022-12-22 21:02:14 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendList from 'components/FriendList/FriendList';
-
 import user from './assets/user.json';
 import data from './assets/data.json';
 import friends from './assets/friends.json';
+import transactions from './assets/transactions.json';
+
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -25,10 +27,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       /> */}
-
       {/* <Statistics title="Upload stats" stats={data} /> */}
-
-      <FriendList friends={friends} />
+      {/* <FriendList friends={friends} /> */}
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
