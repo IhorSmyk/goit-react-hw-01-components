0a4a91e9e0094f50368bb6a2678a4b8beebade0a
Ihor Smyk <ismyk256@gmail.com>
2022-12-24 00:09:15 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,7 @@ import TransactionHistory from './components/TransactionHistory/TransactionHisto
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div>
       {/* <Profile
         username={user.username}
         tag={user.tag}
@@ -29,7 +20,7 @@ export const App = () => {
       /> */}
       {/* <Statistics title="Upload stats" stats={data} /> */}
       {/* <FriendList friends={friends} /> */}
-      <TransactionHistory items={transactions} />;
+      {/* <TransactionHistory items={transactions} />; */}
     </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,31 +1,40 @@
+import PropTypes from 'prop-types';
 
-const TransactionHistory = ({id, type, amiunt, currency }) => {
-    return (
-      <>
-        <table className="transaction-history">
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>Amount</th>
-              <th>Currency</th>
-            </tr>
-          </thead>
+const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-          <tbody>
-            <tr>
-              <td>Invoice</td>
-              <td>125</td>
-              <td>USD</td>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
             </tr>
-            <tr>
-              <td>Withdrawal</td>
-              <td>85</td>
-              <td>USD</td>
-            </tr>
-          </tbody>
-        </table>
-      </>
-    );
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default TransactionHistory;
